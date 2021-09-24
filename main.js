b425bd52f42fc16323dd060fa5da04f3bca7b834
Cristian Descosido <cristiandescosido@gmail.com>
2021-09-24 11:58:28 -0300

    Agregado funcionamiento acordeon

--- main.js
+++ main.js
@@ -4,25 +4,24 @@
 
 let btnItems = document.querySelectorAll('.item .btn-item');
 
-for(let i = 0; i < btnItems.length; i++){
-    btnItems[i].addEventListener('click',function(e){
-        console.log(e.target);
+for (let i = 0; i < btnItems.length; i++) {
+    btnItems[i].addEventListener('click', function (e) {
 
         let btn = e.target;
 
-        if(btn.className == 'btn-item active'){
+        if (btn.className == 'btn-item active') {
             classRemove();
         } else {
             classRemove();
-            btn.classList.add('active');    
+            btn.classList.add('active');
         }
 
-        
+
     })
 }
 
-function classRemove(){
-    for(let i = 0; i < btnItems.length; i++){
+function classRemove() {
+    for (let i = 0; i < btnItems.length; i++) {
         btnItems[i].classList.remove('active');
     }
 }
