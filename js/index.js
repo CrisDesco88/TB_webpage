915a98d4ebf22918cff3bd083ba16b8cbf3422bb
Cristian Descosido <cristiandescosido@gmail.com>
2021-09-26 18:36:28 -0300

    Agregadas animaciones de scroll

---
+++ js/index.js
@@ -0,0 +1,55 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.logo-img', {
+        delay: 1000,
+        rotate: {
+            x: 0,
+            y: 180,
+        },
+        duration: 4000,
+    });
+
+    sr.reveal('.encabezado', {
+        delay: 2500,
+        duration: 3000,
+    });
+
+    sr.reveal('.izq-der', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.izq-der-delay', {
+        delay: 1000,
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.aparecer', {
+        delay: 1000,
+        duration: 4000,
+        distance: '0px',
+        opacity: 0.1
+    });
+
+    sr.reveal('.arriba-abajo', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    
+
+
+
+
+
+
+
