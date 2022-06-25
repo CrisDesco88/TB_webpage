0f7a37c82baba0c1486d326caf977dfec6e479c7
Cristian <cristiandescosido@gmail.com>
2022-06-25 12:45:14 -0300

    mailing disable

--- js/consulta.js
+++ js/consulta.js
@@ -1,22 +1,23 @@
 "use strict";
 
-let btnSubmit = window.document.getElementById("btnSubmit");
-btnSubmit.addEventListener('click',()=>{
-    let nombre = window.document.getElementById('nombreApellido').value;
-    let empresa = window.document.getElementById('inputEmpresa').value;
-    let email = window.document.getElementById('emailInput').value;
-    let telefono = window.document.getElementById('telefonoInput').value;
-    let mensaje = window.document.getElementById('mensajeInput').value;
-    let contactar = window.document.getElementById('gridCheck').value;
+// let btnSubmit = window.document.getElementById("btnSubmit");
+// btnSubmit.addEventListener('click',()=>{
+//     let nombre = window.document.getElementById('nombreApellido').value;
+//     let empresa = window.document.getElementById('inputEmpresa').value;
+//     let email = window.document.getElementById('emailInput').value;
+//     let telefono = window.document.getElementById('telefonoInput').value;
+//     let mensaje = window.document.getElementById('mensajeInput').value;
+//     let contactar = window.document.getElementById('gridCheck').value;
     
     
-    fetch('mail.php?nombre=' + nombre + '&empresa=' + empresa + '&email=' + email + '&telefono=' + telefono + '&mensaje=' + mensaje + '&contactar=' + contactar,{ method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        }
-    }).then(e => {
-        alert(e)
-    });
+//     fetch('mail.php?nombre=' + nombre + '&empresa=' + empresa + '&email=' + email + '&telefono=' + telefono + '&mensaje=' + mensaje + '&contactar=' + contactar,{ method: 'GET',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json',
+//         }
+//     }).then(e => {
+//         alert(e)
+//     })
+//     .catch(e => console.log(e));
 
-});
+// });
